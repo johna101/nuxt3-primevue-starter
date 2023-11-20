fbd931a49f28667258ee64c3a23e8f3486414e69
John Anderson <john@maclarenwest.co.uk>
2023-11-20 21:02:45 +0000

    Tidy up

--- pages/prime/agdatatable.vue
+++ pages/prime/agdatatable.vue
@@ -1,34 +1,39 @@
 <template>
-  Test...
-  <ag-grid-vue
-      style="width: 500px; height: 500px"
-      class="ag-theme-alpine"
-      :column-defs="columnDefs"
-      :row-data="risks"
-      :default-col-def='defaultColDef'
-  />
-  length: {{ risks?.length }}
-  <div>Product length: {{ products?.data?.length }}</div>
-  Risks
+    <div>
+        <ag-grid-vue
+                style="width: 500px; height: 500px"
+                class="ag-theme-alpine"
+                :column-defs="columnDefs"
+                :row-data="risks"
+                :default-col-def='defaultColDef'
+        />
+        length: {{ risks?.length }}
+        <div>
+            Product length: {{ products?.data?.length }}
+        </div>
 
-  <pre>Risk error: {{ riskError}}</pre>
-<!--  <pre>{{ risks}}</pre>-->
-<!--  <pre>{{ mountains}}</pre>-->
+        <pre>Risk error: {{ riskError }}</pre>
+        <!--  <pre>{{ risks}}</pre>-->
+        <!--  <pre>{{ mountains}}</pre>-->
+    </div>
 </template>
 
 <script setup lang="ts">
-import {AgGridVue} from 'ag-grid-vue3'
+import { AgGridVue } from 'ag-grid-vue3'
 
-const {data: products} = await useFetch('/api/products')
+const { data: products } = await useFetch('/api/products')
 
-const {data: risks, error: riskError} = await useFetch(`https://snd-portal-dev.azurewebsites.net/api/referenceData/RiskRatings`)
+const {
+  data: risks,
+  error: riskError
+} = await useFetch('https://snd-portal-dev.azurewebsites.net/api/referenceData/RiskRatings')
 // const {data: risks} = await useAsyncData("risks", () => $fetch(`https://localhost:5010/api/referenceData/ForRiskRatings`))
 // const { risks: any } = await useAsyncData("risk", () => $fetch(`https://www.ag-grid.com/example-assets/row-data.json`))
 
 // const { data: mountains, error } = await useFetch('https://api.nuxtjs.dev/mountains')
 const columnDefs = [
   {headerName: 'Id', field: 'Band'},
-  {headerName: 'Text', field: 'Responsibility'},
+  {headerName: 'Text', field: 'Responsibility'}
 ]
 
 const defaultColDef = {
@@ -41,17 +46,17 @@ const defaultColDef = {
 
   headerComponentParams: {
     template:
-        '<div class="ag-cell-label-container" role="presentation">' +
-        '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>' +
-        '  <div ref="eLabel" class="ag-header-cell-label" role="presentation">' +
-        '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>' +
-        '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>' +
-        '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>' +
-        '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>' +
-        '    <span ref="eText" class="ag-header-cell-text" role="columnheader" style="white-space: normal;"></span>' +
-        '    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
-        '  </div>' +
-        '</div>'
+      '<div class="ag-cell-label-container" role="presentation">' +
+      '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>' +
+      '  <div ref="eLabel" class="ag-header-cell-label" role="presentation">' +
+      '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>' +
+      '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>' +
+      '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>' +
+      '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>' +
+      '    <span ref="eText" class="ag-header-cell-text" role="columnheader" style="white-space: normal;"></span>' +
+      '    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
+      '  </div>' +
+      '</div>'
   }
 
 }
