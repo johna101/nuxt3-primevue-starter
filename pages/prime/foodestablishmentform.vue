13ac4ed33dd252949380e4f0ab0698402a80775a
John Anderson <john@maclarenwest.co.uk>
2023-11-22 11:21:50 +0000

    test pages

---
+++ pages/prime/foodestablishmentform.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card flex flex-wrap gap-12">
+    <FormKit
+        type="form"
+        #default="{value}">
+      <FormKit
+          type="primeInputText"
+          name="name"
+          label="Name"/>
+          <pre>{{ value }}</pre>
+    </FormKit>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+</script>
