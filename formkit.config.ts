7ce13d9697c58d361b3ba6d80028b119fce50ef1
John Anderson <john@maclarenwest.co.uk>
2023-11-24 03:23:59 +0000

    Add formkit pro

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,12 +1,23 @@
 // formkit.config.ts
 import type { DefaultConfigOptions } from '@formkit/vue'
+import {createProPlugin, dropdown, rating, toggle, datepicker} from '@formkit/pro'
 // import { generateClasses } from '@formkit/themes'
-// import '@formkit/themes/genesis'
+import '@formkit/themes/genesis'
+import '@formkit/pro/genesis'
 // import { genesisIcons } from '@formkit/icons'
 import { primeInputs } from '@sfxcode/formkit-primevue'
 import tailwindTheme from "./tailwind-formkit-theme.js"
 
+const proPlugin = createProPlugin('fk-390439ab89', {
+  rating,
+  toggle,
+  dropdown,
+  datepicker
+  // ... and any other Pro Inputs
+})
+
 const config: DefaultConfigOptions = {
+  plugins: [proPlugin],
   // icons: {
   //   ...genesisIcons
   // },
